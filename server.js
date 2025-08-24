7f04eefa7311bfe4385f24f8bfae98418cc1445e
riyabijlani <riyabijlani14@gmail.com>
2025-08-24 12:38:07 -0400

    Add Node/Express server for Render Web Service

---
+++ server.js
@@ -0,0 +1,16 @@
+﻿const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Serve all static files from the repo root
+app.use(express.static(path.join(__dirname)));
+
+// Always return index.html (good for simple SPAs / single page)
+app.get('*', (_req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(LitMatch server running on port );
+});
